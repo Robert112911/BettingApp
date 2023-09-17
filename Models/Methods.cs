67bbb2c8d485c79828e9db5f0383af9befb116e4
Robert Mezei <mezeirobert2004@yahoo.com>
2023-09-17 22:30:11 +0300

    Added input form to Add page and implemented RandomString method

---
+++ Models/Methods.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+namespace BettingApp.Models
+{
+    public class Methods
+    {
+        public static string RandomString(int length)
+        {
+            Random random = new Random();
+            string chars = "abcdefghijklmnopqrstuvwxyz0123456789";
+
+            StringBuilder sb = new StringBuilder(length);
+
+            for (int i = 0; i < length; i++)
+            {
+                sb.Append(chars[random.Next(chars.Length)]);
+            }
+            
+            return sb.ToString();
+        }
+    }
+}
